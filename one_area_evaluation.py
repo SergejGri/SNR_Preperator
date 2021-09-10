847a6cfd9180f69888b7178c8332062b5dcf7dbc
SergejGri <33488921+SergejGri@users.noreply.github.com>
2021-09-10 08:24:29 +0200

    Update one_area_evaluation.py

--- one_area_evaluation.py
+++ one_area_evaluation.py
@@ -46,7 +46,7 @@ def man_SNR_eval_single():
     pixel_size = 4.07033
     pixel_size_units = '$\mu m$'
     nbins = 300
-    path_map = r'C:\Users\Sergej Grischagin\Desktop\Auswertung_SNR\BAD-PIXEL-bin1x1-scans-MetRIC.tif'
+    path_map = r''
 
     dirs = ['140kV', '160kV', '180kV']
     thicknesses = ['2']
@@ -85,7 +85,6 @@ def man_SNR_eval_single():
                                  save_path=os.path.join(path_to_result, f'{dir}_{d}'))
 
 
-
 def man_SNR_eval_multi():
     c_date = datetime.datetime.now()
     _date = f'{c_date.year}-{c_date.month}-{c_date.day}_Evaluation'
@@ -97,7 +96,7 @@ def man_SNR_eval_multi():
     pixel_size = 4.07033
     pixel_size_units = '$\mu m$'
     nbins = 300
-    px_map = r'C:\Users\Sergej Grischagin\Desktop\Auswertung_SNR\BAD-PIXEL-bin1x1-scans-MetRIC.tif'
+    px_map = r''
 
     dirs = ['40kV', '60kV', '80kV', '100kV', '120kV', '140kV', '160kV', '180kV']
     thicknesses = ['4u8', '5u9']
@@ -113,10 +112,10 @@ def man_SNR_eval_multi():
             else:
                 d_l = 'x'
                 d_r = 'x'
-            path_to_data = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\{d}'
-            path_to_refs = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\refs'
-            path_to_darks = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\darks'
-            path_to_result = fr'C:\Users\Sergej Grischagin\Desktop\Auswertung_SNR\{_date}\SNR'
+            path_to_data = fr''
+            path_to_refs = fr''
+            path_to_darks = fr''
+            path_to_result = fr''
             if not os.path.isdir(path_to_result):
                 os.makedirs(path_to_result)
 
@@ -156,8 +155,6 @@ def man_SNR_eval_multi():
                                  save_path=os.path.join(path_to_result, f'{dir}'))
 
 
-
-
 def man_T_eval_single():
     c_date = datetime.datetime.now()
     _date = f'{c_date.year}-{c_date.month}-{c_date.day}_{c_date.hour}_Evaluation'
@@ -175,10 +172,10 @@ def man_T_eval_single():
     thicknesses = ['2']
     for dir in dirs:
         for d in thicknesses:
-            path_to_data = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\{d}'
-            path_to_refs = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\refs'
-            path_to_darks = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\darks'
-            path_to_T_result = fr'C:\Users\Sergej Grischagin\Desktop\Auswertung_SNR\{_date}\Transmission'
+            path_to_data = fr''
+            path_to_refs = fr''
+            path_to_darks = fr''
+            path_to_T_result = fr''
             if not os.path.isdir(path_to_T_result):
                 os.makedirs(path_to_T_result)
 
@@ -200,7 +197,6 @@ def man_T_eval_single():
             with open(os.path.join(_path_T), 'a+') as f_l:
                 f_l.write('{};{}\n'.format(int_vol, T_l))
                 f_l.close()
-
 
 
 def man_T_eval_multi():
@@ -228,10 +224,10 @@ def man_T_eval_multi():
             else:
                 d_l = 'x'
                 d_r = 'x'
-            path_to_data = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\{d}'
-            path_to_refs = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\refs'
-            path_to_darks = fr'\\132.187.193.8\junk\sgrischagin\2021-08-26-Sergej_SNR-Stufenkeil_130proj_6W\{dir}\darks'
-            path_to_T_result = fr'C:\Users\Sergej Grischagin\Desktop\Auswertung_SNR\{_date}\Evaluation'
+            path_to_data = fr''
+            path_to_refs = fr''
+            path_to_darks = fr''
+            path_to_T_result = fr''
             if not os.path.isdir(path_to_T_result):
                 os.makedirs(path_to_T_result)
 
@@ -267,8 +263,6 @@ def man_T_eval_multi():
                 f_r.write('{};{}\n'.format(int_kV, T_r))
                 f_l.close()
                 f_r.close()
-
-
 
 
 def main():
