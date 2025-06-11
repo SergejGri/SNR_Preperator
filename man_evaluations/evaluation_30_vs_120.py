25eb294372e279409e7bd7a3f07d099028b30dd0
SergejGri <33488921+SergejGri@users.noreply.github.com>
2025-06-11 16:53:41 +0200

    Update evaluation_30_vs_120.py
    cleaned up

--- man_evaluations/evaluation_30_vs_120.py
+++ man_evaluations/evaluation_30_vs_120.py
@@ -5,10 +5,11 @@ from image_loader import ImageLoader
 
 
 def evaluate():
-    path_imgs_30 = r'\\132.187.193.8\junk\sgrischagin\2021-12-22-sergej-CT-halbesPhantom-102kV-100ms-5W-M4p46\merged-CT'
-    path_imgs_120 = r'\\132.187.193.8\junk\sgrischagin\2021-12-22-sergej-CT-halbesPhantom-102kV-100ms-5W-M4p46\imgs'
-    path_refs = r'\\132.187.193.8\junk\sgrischagin\2021-12-22-sergej-CT-halbesPhantom-102kV-100ms-5W-M4p46\refs'
-    path_darks = r'\\132.187.193.8\junk\sgrischagin\2021-12-22-sergej-CT-halbesPhantom-102kV-100ms-5W-M4p46\darks'
+    # Please update these paths to your local or project-specific directories
+    path_imgs_30 = 'PATH_TO_IMAGES_30'
+    path_imgs_120 = 'PATH_TO_IMAGES_120'
+    path_refs = 'PATH_TO_REFS'
+    path_darks = 'PATH_TO_DARKS'
 
     view = slice(None, None), slice(50, 945), slice(866, 1040)
     loader = ImageLoader(used_SCAP=False, remove_lines=False, load_px_map=False)
@@ -19,11 +20,11 @@ def evaluate():
     filterer = ImageSeriesPixelArtifactFilterer()
 
     imgs_120 = loader.load_stack(path=path_imgs_120, stack_range=(0, 120))
-    refs_120 = loader.load_stack(path=)
-    darks_120 =
+    refs_120 = loader.load_stack(path=path_refs)
+    darks_120 = loader.load_stack(path=path_darks)
 
-
-    EVA.estimate_SNR(images=)
+    EVA.estimate_SNR(images=imgs_120)
+    # Add the rest of your logic here as needed
 
 
 evaluate()
